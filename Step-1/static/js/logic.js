a6dda7ffabe747e6873257d4e0348e3474741b32
vsk01 <sasivutukuru@gmail.com>
2019-10-02 20:04:59 -0500

    api key desc

--- Step-1/static/js/logic.js
+++ Step-1/static/js/logic.js
@@ -1,4 +1,4 @@
-//api from Mapbox
+//api key to get the maps displayed
 var apiKey = "pk.eyJ1IjoiZmlyZXByb29mc29ja3MiLCJhIjoiY2psd2FpbzdpMTRhczNwcGUxb2phcnRuMCJ9.l4EjUexxcYe9PEifJy2NrA";
 
 //define the map layer
